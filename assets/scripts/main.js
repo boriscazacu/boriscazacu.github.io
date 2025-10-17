245e2146df0b71cabe81c7008a17e8766159b5a4
Boris Cazacu <boris.cazacu@outlook.com>
2025-10-17 10:00:39 +0300

    Add calendar

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,16 +4,26 @@ import { sendData } from "./api.js";
 document.addEventListener("DOMContentLoaded", () => {
     // initTelegram();
 
-    setTimeout(() => {
-        console.log('Timeout')
-        document.getElementById('app-loader-index').classList.remove('active')
-    }, 3100)
+    // setTimeout(() => {
+    //     console.log('Timeout')
+    //     document.getElementById('app-loader-index').classList.remove('active')
+    // }, 3000)
 
-    const button = document.getElementById("main-button");
-    button.addEventListener("click", () => {
-        const data = { action: "book", user: tg.initDataUnsafe.user };
-        // sendData(data);
-        document.getElementById('json-display').textContent = JSON.stringify(tg.initDataUnsafe.user, null, 2);
-
+    const closeModalButton = document.getElementById("close-modal");
+    const modal = document.getElementById("appointment-modal");
+    closeModalButton.addEventListener("click", () => {
+        modal.classList.add("slide-in-bottom");
+        modal.classList.remove("active", "slide-in-top");
+    });
+    
+    document.querySelectorAll('.calendar_item .info').forEach(appointment => {
+        console.log('Adding listener to appointment', appointment);
+        
+        appointment.addEventListener('click', () => {
+            console.log('Appointment clicked');
+            // Here you would typically fetch and display the appointment details
+            modal.classList.remove("active", "slide-in-bottom");
+            modal.classList.add( "slide-in-top", "active");
+        });
     });
 });
