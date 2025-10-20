838780b8962f503f22819ebc4af5a32828368c15
Boris Cazacu <boris.cazacu@outlook.com>
2025-10-20 10:31:58 +0300

    Fix curent time display2

--- assets/scripts/api.js
+++ assets/scripts/api.js
@@ -12,27 +12,42 @@ export async function sendData(data) {
     }
 }
 
-export async function fetchData() {
+export async function fetchData(filters) {
+    console.log("Fetching data with filters:", filters);
+    if ('2025-10-20' !== filters.date) {
+        console.log("No data for the selected date:", filters.date);
+        return [
+            { id: 3, time: "10:00" },
+            { id: 4, time: "10:30" },
+            { id: 14, time: "11:00" },
+            { id: 41, time: "12:30" },
+            { id: 12, time: "13:00" },
+            { id: 9, time: "14:30" },
+            { id: 19, time: "15:00" },
+            { id: 11, time: "16:00" },
+            { id: 13, time: "16:30" },
+        ];
+    }
     return [
         { id: 1, time: "9:00", title: "Tonarea părului / Painting hair", client: "Ion Radu", phone: "093483353", date: "25 Decembrie 2023, 9:00 AM" },
         { id: 2, time: "9:30", title: "Corecția sprâncenelor cu ceară/Коррекция бровей воском", client: "Ion Moldovanu", phone: "093483353", date: "25 Decembrie 2023, 9:30 AM" },
-        { id: 3, time: "10:00"},
-        { id: 4, time: "10:30"},
-        { id: 14, time: "11:00"},
+        { id: 3, time: "10:00" },
+        { id: 4, time: "10:30" },
+        { id: 14, time: "11:00" },
         { id: 5, time: "11:30", title: "Machiaj nunt pentru mireasa", client: "Maria Radu", phone: "093483353", date: "26 Decembrie 2023, 11:30 AM" },
         { id: 6, time: "12:00", title: "Tunsore bărbați /   Men's haircut", client: "Victor Vizitiu", phone: "093483353", date: "26 Decembrie 2023, 12:00 PM" },
-        { id: 41, time: "12:30"},
-        { id: 12, time: "13:00"},
+        { id: 41, time: "12:30" },
+        { id: 12, time: "13:00" },
         { id: 7, time: "13:30", title: "Tunsore bărbați /   Men's haircut", client: "Ion Branza", phone: "093483353", date: "26 Decembrie 2023, 12:00 PM" },
         { id: 8, time: "14:00", title: "Vopsit păr / Hair coloring", client: "Ana Popescu", phone: "093483353", date: "27 Decembrie 2023, 14:00 PM" },
-        { id: 9, time: "14:30"},
-        { id: 19, time: "15:00"},
+        { id: 9, time: "14:30" },
+        { id: 19, time: "15:00" },
         { id: 10, time: "15:30", title: "Manichiură cu gel / Gel manicure", client: "Elena Ionescu", phone: "093483353", date: "27 Decembrie 2023, 15:30 PM" },
-        { id: 11, time: "16:00"},
-        { id: 13, time: "16:30"},
+        { id: 11, time: "16:00" },
+        { id: 13, time: "16:30" },
         { id: 15, time: "17:00", title: "Pedichiură spa / Spa pedicure", client: "Cristina Vasilescu", phone: "093483353", date: "28 Decembrie 2023, 17:00 PM" },
         { id: 16, time: "17:30", title: "Machiaj de zi / Day makeup", client: "Ioana Georgescu", phone: "093483353", date: "28 Decembrie 2023, 17:30 PM" },
         { id: 17, time: "18:00", title: "Coafură pentru ocazii speciale / Hairstyling for special occasions", client: "Gabriela Stan", phone: "093483353", date: "28 Decembrie 2023, 18:00 PM" },
-        { id: 18, time: "18:30"},
+        { id: 18, time: "18:30" },
     ];
 }
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,7 +5,7 @@ const modal = document.getElementById("appointment-modal");
 const closeModalButton = document.getElementById("close-modal");
 const calendar = document.getElementById("calendar");
 const cellHeight = 60 + 7; // 60px height + 7px border-bottom
-let curentDate = new Date();
+let currentDate = new Date();
 
 const clientConfig = {
     timeStep: 30, // minutes
@@ -15,10 +15,9 @@ const clientConfig = {
 
 document.addEventListener("DOMContentLoaded", async () => {
     // initTelegram();
-    const data = await fetchData();
-    populateCalendar(data);
+    await updateCurrentTimeIndicator(0);
 
-    calculateTopOffset();
+    startCalculateTimeOffsetInterval();
 
     closeModalButton.addEventListener("click", () => {
         modal.classList.add("slide-in-bottom");
@@ -26,7 +25,41 @@ document.addEventListener("DOMContentLoaded", async () => {
     });
 });
 
+function startCalculateTimeOffsetInterval() {
+    const now = new Date();
+    const msUntilNextMinute =
+        (60 - now.getSeconds()) * 1000 - now.getMilliseconds();
+
+    // 3️⃣ Wait until the next exact minute, then start syncing every minute
+    setTimeout(() => {
+        calculateTopOffset();
+
+        // Run exactly every minute after that
+        setInterval(calculateTopOffset, 60 * 1000);
+    }, msUntilNextMinute);
+
+}
+
+
+document.getElementById("prev-day-btn").addEventListener("click", (e) => {
+    updateCurrentTimeIndicator(-1);
+});
+
+document.getElementById("next-day-btn").addEventListener("click", (e) => {
+    updateCurrentTimeIndicator(1);
+});
+
+// Method to get calendar data
+async function getCalendarData(filters) {
+    const data = await fetchData(filters);
+    populateCalendar(data);
+}
+
 function calculateTopOffset() {
+    if (!isEqualNow(currentDate)) {
+        console.log("Not today, skipping time indicator update.");
+        return;
+    }
     const root = document.documentElement;
     const now = new Date();
 
@@ -42,13 +75,29 @@ function calculateTopOffset() {
     const offsetMinutes = ((minutes - clientConfig.startTimeInMinutes) / clientConfig.timeStep) * cellHeight;
     console.log("Offset minutes:", offsetMinutes);
 
-    root.style.setProperty('--time-line-position', (offsetMinutes - 25) + 'px');
+    root.style.setProperty('--time-line-position', (offsetMinutes - 24) + 'px');
     calendar.scrollTo(0, offsetMinutes - cellHeight);
     document.querySelector(".calendar_current-time").textContent = now.getHours().toString().padStart(2, '0') + ":" + now.getMinutes().toString().padStart(2, '0');
 }
 
+function isEqualNow(date) {
+    const now = new Date();
+    return date.getFullYear() === now.getFullYear() &&
+        date.getMonth() === now.getMonth() &&
+        date.getDate() === now.getDate();
+}
+
 function populateCalendar(appointments) {
     const calendar = document.getElementById("calendar");
+    calendar.innerHTML = "";
+
+    if (isEqualNow(currentDate)) {
+        const marker = document.createElement("div");
+        marker.className = "calendar_current-time";
+        marker.textContent = new Date().toLocaleTimeString('ro-RO', { hour: '2-digit', minute: '2-digit' });
+        calendar.appendChild(marker);
+    }
+
 
     appointments.forEach(appointment => {
         const isActive = appointment.title && appointment.client;
@@ -90,19 +139,28 @@ function populateCalendar(appointments) {
     });
 }
 
-document.getElementById("prev-day-btn").addEventListener("click", (e) => {
-    updateCurrentTimeIndicator(-1);
-});
+async function updateCurrentTimeIndicator(value) {
+    currentDate.setDate(currentDate.getDate() + value);
+    const today = new Date();
+    let dateString = currentDate.toLocaleDateString('ro-RO', { weekday: "long", day: '2-digit', month: "long" });
 
-document.getElementById("next-day-btn").addEventListener("click", (e) => {
-    updateCurrentTimeIndicator(1);
-});
-
-function updateCurrentTimeIndicator(value) {
-    curentDate.setDate(curentDate.getDate() + value);
-    const dateString = curentDate.toLocaleDateString('ro-RO', { weekday: "long", day: '2-digit', month: "long" });
-    console.log("Updated date:", capitalise(dateString));
+    if (today.getFullYear() === currentDate.getFullYear() && today.getMonth() === currentDate.getMonth()) {
+        if (today.getDate() === currentDate.getDate()) {
+            dateString = `Astăzi, ${currentDate.toLocaleDateString('ro-RO', { day: '2-digit', month: "long" })}`;
+        }
+        const tomorrow = new Date();
+        tomorrow.setDate(today.getDate() + 1);
+        if (tomorrow.getDate() === currentDate.getDate()) {
+            dateString = `Mâine, ${currentDate.toLocaleDateString('ro-RO', { day: '2-digit', month: "long" })}`;
+        }
+        const yesterday = new Date();
+        yesterday.setDate(today.getDate() - 1);
+        if (yesterday.getDate() === currentDate.getDate()) {
+            dateString = `Ieri, ${currentDate.toLocaleDateString('ro-RO', { day: '2-digit', month: "long" })}`;
+        }
+    }
     document.getElementById("currentTimeDisplay").textContent = capitalise(dateString);
+    await getCalendarData({ date: currentDate.toISOString().split('T')[0] });
 }
 
 function capitalise(str) {
