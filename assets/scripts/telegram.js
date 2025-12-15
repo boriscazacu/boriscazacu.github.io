89b1a3b5c1efed1b9bdab1fff28aa31dacc73656
Boris Cazacu <boris.cazacu@outlook.com>
2025-12-15 12:33:16 +0200

    Update info modal

--- assets/scripts/telegram.js
+++ assets/scripts/telegram.js
@@ -2,6 +2,10 @@
 export const tg = window.Telegram.WebApp;
 
 export function initTelegram() {
+    const startParam = tg.initDataUnsafe?.start_param;
+    sessionStorage.setItem("start_param", startParam);
+    document.getElementById('tg-start-key').textContent=startParam
+
     document.getElementById('username-display').textContent = `${tg.initDataUnsafe.user?.first_name || "Guest"} ${tg.initDataUnsafe.user?.last_name || ""}`.trim();
     sessionStorage.setItem("telegram_key", tg?.initDataUnsafe?.user?.id);
 }
