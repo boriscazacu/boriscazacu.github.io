5142bfb0186de684025bc03369459a6f305fe143
Boris Cazacu <boris.cazacu@outlook.com>
2025-12-15 15:39:27 +0200

    Fixes

--- assets/scripts/telegram.js
+++ assets/scripts/telegram.js
@@ -2,9 +2,11 @@
 export const tg = window.Telegram.WebApp;
 
 export function initTelegram() {
+    tg.ready();
+
     const startParam = tg.initDataUnsafe?.start_param;
     sessionStorage.setItem("start_param", startParam);
-    document.getElementById('tg-start-key').textContent=startParam
+    document.getElementById('tg-start-key').textContent=JSON.stringify(tg.initDataUnsafe)
 
     document.getElementById('username-display').textContent = `${tg.initDataUnsafe.user?.first_name || "Guest"} ${tg.initDataUnsafe.user?.last_name || ""}`.trim();
     sessionStorage.setItem("telegram_key", tg?.initDataUnsafe?.user?.id);
